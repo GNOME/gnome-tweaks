79cd36c9ff8f7040229cfce0acd1772d62c40ef4
John Stowers <john.stowers@gmail.com>
2011-03-17 22:32:22 +1300

    Enable text scaling factor

--- gtweak/tweaks/tweak_font.py
+++ gtweak/tweaks/tweak_font.py
@@ -8,7 +8,7 @@ sg = build_horizontal_sizegroup()
 TWEAK_GROUPS = (
         TweakGroup(
             "Fonts",
-#            GSettingsRangeTweak("org.gnome.desktop.interface", "text-scaling-factor", adjustment_step=0.1, size_group=sg),
+            GSettingsRangeTweak("org.gnome.desktop.interface", "text-scaling-factor", adjustment_step=0.1, size_group=sg),
             GSettingsFontButtonTweak("org.gnome.desktop.interface", "font-name", size_group=sg),
             GSettingsFontButtonTweak("org.gnome.desktop.interface", "document-font-name", size_group=sg),
             GSettingsFontButtonTweak("org.gnome.desktop.interface", "monospace-font-name", size_group=sg)),
