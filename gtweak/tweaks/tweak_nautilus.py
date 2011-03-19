12b83cb27e087f940b1a0f8b246779385014ff01
John Stowers <john.stowers@gmail.com>
2011-03-19 17:41:02 +1300

    s/Nautilus/File Manager/

--- gtweak/tweaks/tweak_nautilus.py
+++ gtweak/tweaks/tweak_nautilus.py
@@ -5,7 +5,7 @@ from gtweak.widgets import GSettingsSwitchTweak
 
 TWEAK_GROUPS = (
         TweakGroup(
-            "Nautilus",
+            "File Manager",
             GSettingsSwitchTweak("org.gnome.desktop.background", "show-desktop-icons"),
             GSettingsSwitchTweak("org.gnome.desktop.background", "draw-background")),
 )
