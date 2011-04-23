2dbe05b247196cd105ca45eb312748dd93da88cc
John Stowers <john.stowers@gmail.com>
2011-04-23 18:20:51 +0300

    Remove org.gnome.desktop.background, draw-backgroud

--- gtweak/tweaks/tweak_nautilus.py
+++ gtweak/tweaks/tweak_nautilus.py
@@ -23,6 +23,5 @@ from gtweak.widgets import GSettingsSwitchTweak
 TWEAK_GROUPS = (
         TweakGroup(
             "File Manager",
-            GSettingsSwitchTweak("org.gnome.desktop.background", "show-desktop-icons"),
-            GSettingsSwitchTweak("org.gnome.desktop.background", "draw-background")),
+            GSettingsSwitchTweak("org.gnome.desktop.background", "show-desktop-icons")),
 )
