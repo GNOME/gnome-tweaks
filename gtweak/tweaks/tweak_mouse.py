dcb2ad78e3b4b641f23eec25b3754121cbf9347c
Rui Matos <tiagomatos@gmail.com>
2012-10-06 19:28:57 +0200

    Add a Mouse group with the locate-pointer key
    https://bugzilla.gnome.org/show_bug.cgi?id=685620

---
+++ gtweak/tweaks/tweak_mouse.py
@@ -0,0 +1,9 @@
+from gtweak.tweakmodel import TWEAK_GROUP_MOUSE
+from gtweak.widgets import GSettingsSwitchTweak
+
+TWEAKS = (
+    GSettingsSwitchTweak("org.gnome.settings-daemon.peripherals.mouse",
+                         "locate-pointer",
+                         schema_filename="org.gnome.settings-daemon.peripherals.gschema.xml",
+                         group_name=TWEAK_GROUP_MOUSE),
+)
