8f6e28ee1aa856dfb509ee8978228099161182e5
Alex Muñoz <alexmudoz@gmail.com>
2013-08-08 23:27:47 -0400

    tweak_group_window : Added the title bar buttons.

--- gtweak/tweaks/tweak_group_windows.py
+++ gtweak/tweaks/tweak_group_windows.py
@@ -16,8 +16,25 @@
 # along with gnome-tweak-tool.  If not, see <http://www.gnu.org/licenses/>.
 
 import gtweak
+from gtweak.gshellwrapper import GnomeShellFactory
 from gtweak.tweakmodel import TWEAK_GROUP_WINDOWS
-from gtweak.widgets import ListBoxTweakGroup, GSettingsComboEnumTweak, Title
+from gtweak.widgets import ListBoxTweakGroup, GSettingsComboEnumTweak, GSettingsComboTweak, Title
+
+_shell = GnomeShellFactory().get_shell()
+_shell_loaded = _shell is not None
+
+class ShowWindowButtons(GSettingsComboTweak):
+    def __init__(self, **options):
+        GSettingsComboTweak.__init__(self,
+			"Title Bar Buttons",
+            "org.gnome.desktop.wm.preferences",
+            "button-layout",
+            ((':close', _("Close Only")),
+            (':minimize,close', _("Minimize and Close")),
+            (':maximize,close', _("Maximize and Close")),
+            (':minimize,maximize,close', _("All"))),
+            loaded=_shell_loaded,
+            **options)
 
 TWEAK_GROUPS = [ 
     ListBoxTweakGroup(TWEAK_GROUP_WINDOWS,
@@ -26,6 +43,7 @@ TWEAK_GROUPS = [
         GSettingsComboEnumTweak("Double-click","org.gnome.desktop.wm.preferences", "action-double-click-titlebar"),
         GSettingsComboEnumTweak("Middle-click","org.gnome.desktop.wm.preferences", "action-middle-click-titlebar"),
         GSettingsComboEnumTweak("Secondary-click","org.gnome.desktop.wm.preferences", "action-right-click-titlebar"),
+        ShowWindowButtons(group_name=TWEAK_GROUP_WINDOWS),
     )
 ]
 
