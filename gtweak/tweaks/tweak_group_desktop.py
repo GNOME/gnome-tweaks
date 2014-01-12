a1c0570d41461c37ec276064b016a4718c0c88a5
Alex Muñoz <amunoz@src.gnome.org>
2014-01-12 09:58:17 -0300

    Add Lock Screen Background
    https://bugzilla.gnome.org/show_bug.cgi?id=709877

--- gtweak/tweaks/tweak_group_desktop.py
+++ gtweak/tweaks/tweak_group_desktop.py
@@ -32,6 +32,9 @@ TWEAK_GROUPS = [
         Title(_("Background"), "", uid="title-theme"),
         GSettingsComboEnumTweak(_("Mode"),"org.gnome.desktop.background", "picture-options"),
         GSettingsFileChooserButtonTweak(_("Background Location"),"org.gnome.desktop.background", "picture-uri", local_only=True, mimetypes=["application/xml","image/png","image/jpeg"]),
+        Title(_("Lock Screen"), "", uid="title-theme"),
+        GSettingsComboEnumTweak(_("Mode"),"org.gnome.desktop.screensaver", "picture-options"),
+        GSettingsFileChooserButtonTweak(_("Lock Screen Location"),"org.gnome.desktop.screensaver", "picture-uri", local_only=True, mimetypes=["application/xml","image/png","image/jpeg"]),
         #Title(_("Files"), ""),
         #GSettingsSwitchTweak(_("Use location entry"), "org.gnome.nautilus.preferences", "always-use-location-entry",schema_filename="org.gnome.nautilus.gschema.xml"),
 
