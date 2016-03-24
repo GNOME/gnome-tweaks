a8bbca4383bec790ec57dc172672a547638734c2
Patrick Griffis <tingping@tingping.se>
2016-03-23 20:39:20 -0400

    Support versioned Gtk3 themes

--- gtweak/tweaks/tweak_group_interface.py
+++ gtweak/tweaks/tweak_group_interface.py
@@ -46,12 +46,17 @@ class GtkThemeSwitcher(GSettingsComboTweak):
 
     def _get_valid_themes(self):
         """ Only shows themes that have variations for gtk+-3 and gtk+-2 """
+        gtk_ver = Gtk.MINOR_VERSION
+        if gtk_ver % 2: # Want even number
+            gtk_ver += 1
+
         dirs = ( os.path.join(gtweak.DATA_DIR, "themes"),
                  os.path.join(GLib.get_user_data_dir(), "themes"),
                  os.path.join(os.path.expanduser("~"), ".themes"))
         valid = walk_directories(dirs, lambda d:
                     os.path.exists(os.path.join(d, "gtk-2.0")) and \
-                        os.path.exists(os.path.join(d, "gtk-3.0")))
+                        (os.path.exists(os.path.join(d, "gtk-3.0")) or \
+                         os.path.exists(os.path.join(d, "gtk-3.{}".format(gtk_ver)))))
         return valid
 
 class IconThemeSwitcher(GSettingsComboTweak):
