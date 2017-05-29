e3249e58a202155213b0b89ee14c016ce1190648
Jeremy Bicha <jbicha@ubuntu.com>
2017-05-29 12:32:45 -0400

    workspaces: Partially implement mockup from Design Team
    https://raw.githubusercontent.com/gnome-design-team/gnome-mockups/master/tweak-tool/tweak-tool-wires.png
    
    https://bugzilla.gnome.org/show_bug.cgi?id=783176

--- gtweak/tweaks/tweak_group_workspaces.py
+++ gtweak/tweaks/tweak_group_workspaces.py
@@ -59,6 +59,7 @@ TWEAK_GROUPS = [
     ListBoxTweakGroup(TWEAK_GROUP_WORKSPACES,
         sw,
         GSettingsSpinButtonTweak(_("Number of Workspaces"), "org.gnome.desktop.wm.preferences", "num-workspaces", depends_on = sw, depends_how=depends_how, size_group=sg),
-        GSettingsSwitchTweak(_("Workspaces only on primary display"),"org.gnome.mutter", "workspaces-only-on-primary", schema_filename="org.gnome.shell.gschema.xml", loaded=_shell_loaded),
+        Title(_("Display Handling"), "", uid="title-theme"),
+        GSettingsSwitchTweak(_("Workspaces on primary display only"),"org.gnome.mutter", "workspaces-only-on-primary", schema_filename="org.gnome.shell.gschema.xml", desc=_("Additional displays are treated as independent workspaces."),loaded=_shell_loaded),
     )
 ]
