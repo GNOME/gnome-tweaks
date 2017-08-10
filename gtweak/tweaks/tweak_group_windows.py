d6c4da33d65ba7a100c02f3da0a3cb137d97136d
Jeremy Bicha <jbicha@ubuntu.com>
2017-08-10 15:49:36 -0400

    Revert "windows: Revert to 3.24 Focus Mode combobox"
    This reverts commit ca0166cf626474ad1ae654e18d5d625b05e9728d.
    
    Now that the vertical scrollbar doesn't look so bad

--- gtweak/tweaks/tweak_group_windows.py
+++ gtweak/tweaks/tweak_group_windows.py
@@ -25,12 +25,7 @@ import gettext
 from gi.repository import Gio, Gtk, GLib
 
 
-class Focus(GSettingsComboEnumTweak):
-    def __init__(self, **options):
-        GSettingsComboEnumTweak.__init__(self, _("Focus Mode"), "org.gnome.desktop.wm.preferences", "focus-mode")
-
-
-class NewFocusListBox(Gtk.ListBox, Tweak):
+class Focus(Gtk.ListBox, Tweak):
 
     def __init__(self, **options):
         Gtk.ListBox.__init__(self)
