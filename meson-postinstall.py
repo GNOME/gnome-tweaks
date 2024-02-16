eec610429bcf04fbde31ecb214b4e5d4508654d2
Bobby Rong <rjl931189261@126.com>
2024-02-16 11:50:25 +0800

    meson: Use gnome.post_install
    Which calls gtk4-update-icon-cache if available.
    Fixes the build when gtk3 is not in builder sandbox.

--- meson-postinstall.py
+++ meson-postinstall.py
@@ -3,18 +3,9 @@
 import sysconfig
 from compileall import compile_dir
 from os import environ, path
-from subprocess import call
 
 prefix = environ.get('MESON_INSTALL_PREFIX', '/usr/local')
-datadir = path.join(prefix, 'share')
 destdir = environ.get('DESTDIR', '')
-
-# Package managers set this so we don't need to run
-if not destdir:
-    print("Installing new Schemas")
-    call(['glib-compile-schemas', path.join(datadir, 'glib-2.0/schemas')])
-    print('Updating icon cache...')
-    call(['gtk-update-icon-cache', '-qtf', path.join(datadir, 'icons', 'hicolor')])
 
 print('Compiling python bytecode...')
 moduledir = sysconfig.get_path('purelib', vars={'base': str(prefix)})
